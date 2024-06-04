6354a07c69cb7e07390a9b29e9407939f4a9ab39
ibnatysalsabila <salsajpr01@gmail.com>
2024-06-04 17:42:04 +0700

    membuka file dalam mode membaca dan menunjukan ke sebuah file

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -26,4 +26,9 @@ int main() {
 	// selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
+	// Membuka file dalam mode membaca
+	ifstream infile;
+	// menunjukan ke sebuah file
+	infile.open("contohfile.txt");
+
 	
