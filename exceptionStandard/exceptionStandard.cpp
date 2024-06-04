3ad010e24a26f37494e93e3e39b14ab042deab93
ibnatysalsabila <salsajpr01@gmail.com>
2024-06-04 18:36:11 +0700

    menambahkan penangkap pada obyek exception dan menampilkan Baris Program Yang Terakhir

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -14,4 +14,12 @@ int main()
 		cout << data.at(5) << endl;
 		//memanggil array ke elemen 5
 	}
-	
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+	}
+	cout << "Baris Program Yang Terakhir" << endl;
+	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+	return 0;
+}
